42e155bd0c563a60a2037c7c5ac41f71d73ff615
Edison Pizarro Godoy <edison.pizarro.godoy@gmail.com>
2023-06-09 13:55:56 -0400

    Add comments and EC2 improvements

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,4 +1,14 @@
+# AWS Docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+
+# Steps:
+# 0. Select a provider                                                          https://registry.terraform.io/providers/hashicorp/aws/latest
+# 1. Create an EC2 instance                                                     https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+# 2. Create Elastic IP and assign                                               https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eip
+# 3. Create a security group to open ports                                      https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+# optional. Define output to display after apply                                https://developer.hashicorp.com/terraform/language/values/outputs
+
 terraform {
+  # All providers: https://registry.terraform.io/browse/providers?product_intent=terraform
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -9,34 +19,32 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+# 0. Select a provider 
 provider "aws" {
   region = "us-east-1"
 }
 
-# Create a new EC2 instance
+# 1. Create a new EC2 instance
 resource "aws_instance" "my_instance" {
-  ami           = "ami-053b0d53c279acc90" # AMI ID for Ubuntu 20.04 LTS (free tier)
+  ami           = "ami-053b0d53c279acc90" # AMI ID for Ubuntu 20.04 LTS (free tier) # Find an AMI: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/finding-an-ami.html
   instance_type = "t2.micro"
   #   key_name      = aws_key_pair.my_key_pair.key_name
-  key_name               = "ayudantia-iaac-terraform"
+  key_name               = "iic2173"
   vpc_security_group_ids = [aws_security_group.my_security_group.id]
 
-
+  # user_data is used to run an script after provisioning the instance
+  user_data = "${file("./scripts/deployment.sh")}"
   tags = {
     Name = "ayudantia-iaac"
   }
 }
 
+# 2. Create and assign Elastic IP
 resource "aws_eip" "my_eip" {
   instance = aws_instance.my_instance.id
 }
 
-resource "aws_eip_association" "my_eip_association" {
-  instance_id   = aws_instance.my_instance.id
-  allocation_id = aws_eip.my_eip.id
-}
-
-# Create a security group
+# 3. Create a security group to open ports
 resource "aws_security_group" "my_security_group" {
   name        = "my-security-group"
   description = "Security group for SSH access"
@@ -47,8 +55,21 @@ resource "aws_security_group" "my_security_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
 
+# Output to dislay after `terraform apply`
 output "elastic_ip" {
   value = aws_eip.my_eip.public_ip
+}
+
+output "ssh_command" {
+  value = "ssh -i ${aws_instance.my_instance.key_name}.pem ubuntu@${aws_eip.my_eip.public_ip}"
 }
