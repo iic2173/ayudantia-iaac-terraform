ff6dc29089780cceafa0e43b95a3e1c37963546e
Edison Pizarro Godoy <edison.pizarro.godoy@gmail.com>
2023-06-09 14:23:33 -0400

    Add nginx

--- ec2_full_network_conf/main.tf
+++ ec2_full_network_conf/main.tf
@@ -129,10 +129,10 @@ resource "aws_instance" "main" {
 }
 
 # Output to dislay after `terraform apply`
-output "elastic_ip" {
-  value = aws_eip.main.public_ip
-}
+  output "elastic_ip" {
+    value = aws_eip.main.public_ip
+  }
 
-output "ssh_command" {
-  value = "ssh -i ${aws_instance.main.key_name}.pem ubuntu@${aws_eip.main.public_ip}"
-}
+  output "ssh_command" {
+    value = "ssh -i ${aws_instance.main.key_name}.pem ubuntu@${aws_eip.main.public_ip}"
+  }
